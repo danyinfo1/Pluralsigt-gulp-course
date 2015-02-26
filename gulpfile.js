7ddb2e963490c8ded7f1cf76abeba5a48dc9e007
unknown <dmena@CR7NCC.lan.trintech.com>
2015-02-26 15:06:59 -0600

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
+(function () {
+    
+    var gulp = require('gulp'), args = require('yargs').argv;
+    var $ = require('gulp-load-plugins')({lazy: true});
+    var config = require('./gulp.config')();
 
-gulp.task('vet', function () {
-    return gulp
-    .src([
-        './src/**/*.js',
-        './*.js'
-    ])
-    .pipe(jscs())
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
-});
+
+    function log(msg) {
+        var item;
+        if (typeof (msg) === 'object') {
+            for (item in msg)
+                {
+                    if (msg.hasOwnProperty(item)) {
+                        $.util.log($.util.colors.blue(msg[item]));
+                    }
+                }
+        } else {
+            $.util.log($.util.colors.blue(msg));
+        }
+    }
+
+    gulp.task('vet', function () {
+
+        log('Analizing source with JSHint and JSCS');
+
+        return gulp
+            .src(config.alljs)
+            .pipe($.if(args.verbose, $.print()))
+            .pipe($.jscs())
+            .pipe($.jshint())
+            .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+            .pipe($.jshint.reporter('fail'));
+    });
+}());
